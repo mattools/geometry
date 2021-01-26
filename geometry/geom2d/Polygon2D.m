33179550464c760f29cc2ce1dad9a6d5696613eb
dlegland <david.legland@inrae.fr>
2021-01-26 18:39:45 +0100

    add Polygon2D.convexHull

--- geometry/geom2d/Polygon2D.m
+++ geometry/geom2d/Polygon2D.m
@@ -33,6 +33,74 @@ methods (Static)
         %
         poly = SimplePolygon2D(coords);
     end
+    
+    function [hull, inds] = convexHull(varargin)
+        % Compute the convex hull of the set of input points.
+        %
+        % This function is mainly a wrapper to the convhull function, that
+        % format the result to a Polygon2D.
+        %
+        % Example
+        %   % Generate a point set, and compute its convex hull
+        %   coords = [10 30;10 60;30 20; 30 40; 30 70; 40 80; 50 40; 50 60; 70 30; 70 50; 80 70];
+        %   pts = MultiPoint2D(coords);
+        %   figure; draw(pts, 'bo'); axis([0 100 0 100]); hold on;
+        %   hull = Polygon2D.convexHull(pts);
+        %   draw(hull, 'b');
+        
+        % get coordinates of points
+        if nargin == 1
+            var1 = varargin{1};
+            if isa(var1, 'MultiPoint2D')
+                points = var1.Coords;
+            elseif isnumeric(var1) && size(var1, 2) == 2
+                points = var1;
+            else
+                error('Input argument must be either numeric or a MultiPoint2D');
+            end
+            
+        elseif nargin > 1
+            np = length(varargin);
+            points = zeros(np, 2);
+            
+            for i = 1:np
+                arg = varargin{i};
+                if isnumeric(arg)
+                    points(i, :) = arg;
+                elseif isa(arg, 'Point2D')
+                    points(i, :) = [arg.X arg.Y];
+                else
+                    error('Can not interpret input argument');
+                end
+            end
+        else
+            error('Requires at least one input argument');
+        end
+        
+        % checkup on array size
+        if size(points, 1) < 3
+            hull = points;
+            inds = 1:size(points, 1);
+            return;
+        end
+        
+        % parse simplify option
+        simplify = true;
+%         if nargin > 2 && strcmpi(varargin{1}, 'simplify')
+%             simplify = varargin{2};
+%         end
+        
+        % compute convex hull by calling the 'convhull' function
+        inds = convhull(points(:,1), points(:,2), 'simplify', simplify);
+        
+        % avoid considering the same vertex at the beginning and at the end
+        if inds(1) == inds(end)
+            inds(end) = [];
+        end
+        
+        % create polygon
+        hull = SimplePolygon2D(points(inds, :));
+    end
 end
 
 
--- tests/geom2d/test_Polygon2D.m
+++ tests/geom2d/test_Polygon2D.m
@@ -27,3 +27,24 @@ poly = Polygon2D.create(vertices);
 
 assertTrue(testCase, isa(poly, 'Polygon2D'));
 assertEqual(testCase, size(vertexCoordinates(poly), 1), 4);
+
+
+function test_convexHull_points(testCase) %#ok<*DEFNU>
+
+vertices = [16 14; 10 10; 16 17; 20 10; 13 12; 20 20; 14 18; 10 20; 15 15;];
+
+poly = Polygon2D.convexHull(vertices);
+
+assertTrue(testCase, isa(poly, 'Polygon2D'));
+assertEqual(testCase, size(vertexCoordinates(poly), 1), 4);
+
+
+function test_convexHull_MultiPoint2D(testCase) %#ok<*DEFNU>
+
+vertices = [16 14; 10 10; 16 17; 20 10; 13 12; 20 20; 14 18; 10 20; 15 15;];
+pts = MultiPoint2D(vertices);
+
+poly = Polygon2D.convexHull(pts);
+
+assertTrue(testCase, isa(poly, 'Polygon2D'));
+assertEqual(testCase, size(vertexCoordinates(poly), 1), 4);
