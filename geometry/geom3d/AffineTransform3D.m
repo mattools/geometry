e3c60cf8b8b2fa88e4c56510a99947d7ded5eba9
dlegland <david.legland@inrae.fr>
2021-04-29 11:33:13 +0200

    geom3d/AffineTransform3D: implements 3D rotations

--- geometry/geom3d/AffineTransform3D.m
+++ geometry/geom3d/AffineTransform3D.m
@@ -4,14 +4,21 @@ classdef AffineTransform3D < handle
 %   Class AffineTransform3D
 %
 %   Example
-%     T = AffineTransform3D.createScaling([3 2 1]);
+%     S = AffineTransform3D.createScaling([1.5 1 0.75]);
+%     R = AffineTransform3D.createRotationOx(pi/6);
+%     T = AffineTransform3D.createTranslation([30 20 10]);
+%     transfo = T * R * S;
+%     pts = MultiPoint3D(randn(50, 3));
+%     pts2 = transform(pts, transfo);
+%     figure; hold on; axis equal; axis([0 100 0 100 0 100]);
+%     draw(pts2, 'b*');
 %
 %   See also
 %     AffineTransform2D
 
 % ------
 % Author: David Legland
-% e-mail: david.legland@inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2019-04-03,    using Matlab 9.5.0.944444 (R2018b)
 % Copyright 2019 INRA - BIA-BIBS.
 
@@ -28,7 +35,9 @@ end % end properties
 methods (Static)
     function obj = createTranslation(shift, varargin)
         % Create a new 3D affine transform representing a translation.
-        % trans = AffineTransform2D.createTranslation([dx dy])
+        %
+        % trans = AffineTransform3D.createTranslation([DX DY DZ])
+        %
         if isnumeric(shift)
             if all(size(shift) == [1 3])
                 dx = shift(1);
@@ -50,8 +59,8 @@ methods (Static)
         % Create a new affine transform representing a scaling.
         %
         % Usage:
-        % TRANS = AffineTransform2D.createScaling(S);
-        % TRANS = AffineTransform2D.createScaling([SX SY SZ]);
+        % TRANS = AffineTransform3D.createScaling(S);
+        % TRANS = AffineTransform3D.createScaling([SX SY SZ]);
         %
         % Example
         %     T = AffineTransform3D.createScaling([3 2 1]);
@@ -67,6 +76,181 @@ methods (Static)
         end
         obj = AffineTransform3D([sx 0 0 0   0 sy 0 0   0 0 sz 0]);
     end
+    
+    function obj = createRotationOx(varargin)
+        % Create a 3D rotation around the X-axis.
+        %
+        % Usage:
+        % TRANS = AffineTransform3D.createRotationOx(THETA);
+        % Returns the transform corresponding to a rotation by the angle
+        % THETA (in radians) around the Ox axis. A rotation by an angle of
+        % PI/2 would transform the vector [0 1 0] into the vector [0 0 1]. 
+        %
+        % Example
+        %     R = AffineTransform3D.createRotationOx(pi/6);
+        %
+        %   See also:
+        %   transformPoint, createRotationOy, createRotationOz
+        
+        % default center value
+        dx = 0; dy = 0; dz = 0;
+        
+        % get input values
+        if length(varargin) == 1
+            % only one argument -> rotation angle
+            theta = varargin{1};
+            
+        elseif length(varargin) == 2
+            % origin point and angle
+            center = varargin{1};
+            if isnumeric(center)
+                dx = center(1);
+                dy = center(2);
+                dz = center(3);
+            elseif isa(center, 'Point3D')
+                dx = center.X;
+                dy = center.Y;
+                dz = center.Z;
+            end
+            theta = varargin{2};
+        else
+            error('Wrong number of input arguments');
+        end
+        
+        % compute coefs
+        cot = cos(theta);
+        sit = sin(theta);
+        
+        % create transformation
+        rotMat = [...
+            1   0   0   0;...
+            0 cot -sit  0;...
+            0 sit  cot  0;...
+            0   0   0   1];
+        
+        % change center of transformation
+        t = [1 0 0 dx; 0 1 0 dy; 0 0 1 dz; 0 0 0 1];
+        rotMat = t * rotMat / t;
+        
+        obj = AffineTransform3D(rotMat);
+    end
+    
+    function obj = createRotationOy(varargin)
+        % Create a 3D rotation around the Y-axis.
+        %
+        % Usage:
+        % TRANS = AffineTransform3D.createRotationOy(THETA);
+        % Returns the transform corresponding to a rotation by the angle
+        % THETA (in radians) around the Oy axis. A rotation by an angle of
+        % PI/2 would transform the vector [0 0 1] into the vector [1 0 0]. 
+        %
+        % Example
+        %     R = AffineTransform3D.createRotationOy(pi/6);
+        %
+        %   See also:
+        %   transformPoint, createRotationOx, createRotationOz
+
+        % default center value
+        dx = 0; dy = 0; dz = 0;
+        
+        % get input values
+        if length(varargin) == 1
+            % only one argument -> rotation angle
+            theta = varargin{1};
+            
+        elseif length(varargin) == 2
+            % origin point and angle
+            center = varargin{1};
+            if isnumeric(center)
+                dx = center(1);
+                dy = center(2);
+                dz = center(3);
+            elseif isa(center, 'Point3D')
+                dx = center.X;
+                dy = center.Y;
+                dz = center.Z;
+            end
+            theta = varargin{2};
+        else
+            error('Wrong number of input arguments');
+        end
+        
+        % compute coefs
+        cot = cos(theta);
+        sit = sin(theta);
+        
+        % create transformation
+        rotMat = [...
+            cot  0  sit  0;...
+            0    1    0  0;...
+            -sit 0  cot  0;...
+            0    0    0  1];
+        
+        % change center of transformation
+        t = [1 0 0 dx; 0 1 0 dy; 0 0 1 dz; 0 0 0 1];
+        rotMat = t * rotMat / t;
+        
+        obj = AffineTransform3D(rotMat);
+    end
+
+    function obj = createRotationOz(varargin)
+        % Create a 3D rotation around the Z-axis.
+        %
+        % Usage:
+        % TRANS = AffineTransform3D.createRotationOz(THETA);
+        % Returns the transform corresponding to a rotation by the angle
+        % THETA (in radians) around the Ox axis. A rotation by an angle of
+        % PI/2 would transform the vector [1 0 0] into the vector [0 1 0]. 
+        %
+        % Example
+        %     R = AffineTransform3D.createRotationOx(pi/6);
+        %
+        %   See also:
+        %   transformPoint, createRotationOx, createRotationOy
+        
+        % default center value
+        dx = 0; dy = 0; dz = 0;
+        
+        % get input values
+        if length(varargin) == 1
+            % only one argument -> rotation angle
+            theta = varargin{1};
+            
+        elseif length(varargin) == 2
+            % origin point and angle
+            center = varargin{1};
+            if isnumeric(center)
+                dx = center(1);
+                dy = center(2);
+                dz = center(3);
+            elseif isa(center, 'Point3D')
+                dx = center.X;
+                dy = center.Y;
+                dz = center.Z;
+            end
+            theta = varargin{2};
+        else
+            error('Wrong number of input arguments');
+        end
+        
+        % compute coefs
+        cot = cos(theta);
+        sit = sin(theta);
+        
+        % create transformation
+        rotMat = [...
+            cot -sit 0 0;...
+            sit  cot 0 0;...
+             0    0  1 0;...
+             0    0  0 1];
+        
+        % change center of transformation
+        t = [1 0 0 dx; 0 1 0 dy; 0 0 1 dz; 0 0 0 1];
+        rotMat = t * rotMat / t;
+        
+        obj = AffineTransform3D(rotMat);
+    end
+
     
     function obj = identity()
         % Create a new 3D affine transform representing identity.
@@ -179,7 +363,7 @@ methods (Static)
         if exist('loadjson', 'file') == 0
             error('Requires the ''jsonlab'' library');
         end
-        box = AffineTransform2D.fromStruct(loadjson(fileName));
+        box = AffineTransform3D.fromStruct(loadjson(fileName));
     end
     
     function transfo = fromStruct(str)
